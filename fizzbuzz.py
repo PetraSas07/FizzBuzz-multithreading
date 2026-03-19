fda036a1cdeeb3fc7fca53b15bc8022dc513d4c1
PetraSas07 <saspetra07@gmail.com>
2026-03-19 15:32:31 +0100

    cleaned up fizzbuzz into a class

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,29 @@
 import threading
 
-def fizzbuzz(n):
+class Fizzbuzz:
+    def __init__(self, n):
+        result = []
+        [result.append(str(i)) for i in range(1, n + 1)]
+        self.list = result
+        self.size = n
+
+    def fizzbuzz(self, x, y):
+        z = x + y
+        for i in range(self.size):
+            if ((i % z) == 0):
+                self.list[i] = "FizzBuzz"
+
+    def fizz(self, x):
+        for i in range(self.size):
+            if ((i % x) == 0):
+                self.list[i] = "Fizz"
+
+    def buzz(self, y):
+        for i in range(self.size):
+            if ((i % y) == 0):
+                self.list[i] = "Buzz"
+
+    
     result = []
     if n >= 1:
         for i in range(1, n + 1):
