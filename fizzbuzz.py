a5271b3d6ecc71bad6209ab4053ffb256778dd8a
PetraSas07 <saspetra07@gmail.com>
2026-03-19 16:57:57 +0100

    arranged so the threads have to collide

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,30 @@
-class Fizzbuzz:
+import time as t
+class FizzBuzz:
     def __init__(self, n):
-        result = []
-        [result.append(str(i)) for i in range(1, n + 1)]
-        self.list = result
+        self.string_list = []
         self.size = n
+        
+    @property
+    def index(self):
+        return len(self.string_list)
+        
 
     def fizzbuzz(self, x, y):
-        z = x + y
-        for i in range(self.size):
+        z = x * y
+        for i in range(1, self.size + 1):
             if ((i % z) == 0):
-                self.list[i] = "FizzBuzz"
+                self.string_list.append("FizzBuzz")
 
     def fizz(self, x):
-        for i in range(self.size):
+        for i in range(1, self.size + 1):
             if ((i % x) == 0):
-                self.list[i] = "Fizz"
+                self.string_list.append("Fizz")
 
     def buzz(self, y):
-        for i in range(self.size):
+        for i in range(1, self.size + 1):
             if ((i % y) == 0):
-                self.list[i] = "Buzz"
+                self.string_list.append("Buzz")
+
+    def number(self):
+        for i in range(1, self.size + 1):
+            self.string_list.append(str(i))
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from fizzbuzz import Fizzbuzz as F
+from fizzbuzz import FizzBuzz
 import threading
 
 def main():
     # Create original instance to work on
-    line = F(16)
+    fizzbuzz_list = FizzBuzz(16)
     # Create threads
-    thread_A = threading.Thread(target= line.fizzbuzz, args= (4, 6))
-    thread_B = threading.Thread(target= line.fizz, args= (4,))
-    thread_C = threading.Thread(target= line.buzz, args= (6,))
+    thread_A = threading.Thread(target= fizzbuzz_list.number)
+    thread_B = threading.Thread(target= fizzbuzz_list.fizzbuzz, args= (3, 5))
+    thread_C = threading.Thread(target= fizzbuzz_list.fizz, args= (3,))
+    thread_D = threading.Thread(target= fizzbuzz_list.buzz, args= (5,))
+    
 
     # Start the threads
+    
     thread_A.start()
     thread_B.start()
     thread_C.start()
+    thread_D.start()
 
     # Join the threads
     thread_A.join()
     thread_B.join()
     thread_C.join()
+    thread_D.join()
+
+    print(fizzbuzz_list.string_list)
 
 
 if __name__ == "__main__":
