5aa22ec370363a9e6917dc7a7ae2ff2f6391b2f6
PetraSas07 <saspetra07@gmail.com>
2026-03-19 15:08:23 +0100

    implemented fizzbuzz logic

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,14 @@ import threading
 
 def fizzbuzz(n):
     result = []
+    if n >= 1:
+        for i in range(1, n + 1):
+            if (i % 15) == 0:
+                result.append("FizzBuzz")
+            elif (i % 3) == 0:
+                result.append("Fizz")
+            elif (i % 5) == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
     return result
